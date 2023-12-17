3fe4cce6e199800203738d77719af204b71555dc
Bruno Rossi <brunogabrielrossi@gmail.com>
2023-12-17 13:12:20 -0500

    Effects and timeout lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+
+    // Reset time to 10 if the timer runs out, and set the answer to false:
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return;
+    }
+
+    // Use setTimeout method and run function to reduce timeRemaining by 1 every second until 0:
+    const timerId = setTimeout(() => {
+      setTimeRemaining((timeRemaining) => timeRemaining - 1);
+    }, 1000);
+
+    // Use clearTimeout method on timerId as a cleaning function:
+    return function () {
+      clearTimeout(timerId);
+    };
+
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
